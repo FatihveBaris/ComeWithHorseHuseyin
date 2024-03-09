4dc6917a51449625ac024b4abfe601e899096321
Dilara <dilaraozturk5881@gmail.com>
2024-03-09 16:39:53 +0300

    CREDITS added

--- Assets/Scripts/Main_menu.cs
+++ Assets/Scripts/Main_menu.cs
@@ -7,8 +7,9 @@ using UnityEngine.SceneManagement;
 public class Main_menu : MonoBehaviour
 {
     public void StartButton()
-    {
+    {   //Daha belli deðil diye default deðer olarak 1 atadým.
 
+        SceneManager.LoadScene(1);
     }
 
     public void SetttingsButton()
