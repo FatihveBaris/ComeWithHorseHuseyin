36724223fad0f23c016ca6b7c48088aab7f1cbf9
Dilara <dilaraozturk5881@gmail.com>
2024-03-09 15:59:27 +0300

    settings added

--- Assets/Scripts/Main_menu.cs
+++ Assets/Scripts/Main_menu.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Main_menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartButton()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetttingsButton()
+    {
+
+    }
+
+    public void CreditsButton()
+    {
+
+    }
+
+    public void QuitButton()
     {
         
     }
