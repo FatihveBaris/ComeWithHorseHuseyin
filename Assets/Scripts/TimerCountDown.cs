6500107b14ade4fe486e9963f9b767461ee6a4ad
FatihveBaris <117710225+FatihveBaris@users.noreply.github.com>
2024-03-09 22:10:45 +0300

    timer added

--- Assets/Scripts/TimerCountDown.cs
+++ Assets/Scripts/TimerCountDown.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // UI kullanmak için gerekli kütüphane
+using TMPro;
 
-public class TimerCountDown : MonoBehaviour
+public class Countdown : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float timeLeft = 3.0f; // Geri sayım süresi
+    public TextMeshProUGUI countdownText; // Geri sayımı gösterecek UI Text nesnesi
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;// Her frame'de süreyi azalt
+        if (timeLeft > 0)
+        {
+            countdownText.text = Mathf.Ceil(timeLeft).ToString(); // Süreyi yuvarlayıp string'e çevir ve ekranda göster
+        }
+        else
+        {
+            countdownText.text = "Başla!";// Süre bittiğinde ekranda ne gösterileceği
+            
+            if (timeLeft < -2)
+            {
+                countdownText.text = " ";
+            }
+        }
     }
 }
