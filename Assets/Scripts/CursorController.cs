5745e0a9af10d7f38ec7cb795b4d0ede98338cab
FatihveBaris <117710225+FatihveBaris@users.noreply.github.com>
2024-03-09 14:57:27 +0300

    CursorController Update

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -8,23 +8,29 @@ public class CursorController : MonoBehaviour
     public Texture2D cursor;
     public Texture2D activeCursor;
     private Camera _mainCamera;
+    private InputAction _mouseAction;
+
     private void Awake()
     {
         _mainCamera = Camera.main;
+        _mouseAction = new InputAction(type: InputActionType.PassThrough, binding: "<Mouse>/position");
+        _mouseAction.performed += context => CheckForCollider(context.ReadValue<Vector2>());
+        _mouseAction.Enable();
     }
-    
-    public void OnMouseOver(InputAction.CallbackContext context)
+
+    private void CheckForCollider(Vector2 mousePosition)
     {
-        if (!context.started) return;
-
-        var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay((Vector3)Mouse.current.position.ReadValue()));
-        if (rayHit.collider == null) SetNormalCursor();
-
-        Debug.Log(rayHit.collider.gameObject.name);
+        var rayHit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(mousePosition));
+        if (rayHit.collider != null)
+        {
+            SetActiveCursor();
+        }
+        else
+        {
+            SetNormalCursor();
+        }
     }
-    
-    
-    
+
     void Start() {
         // Cursor'u göz yapıyoruz. 
         Cursor.SetCursor(cursor, Vector3.zero, CursorMode.ForceSoftware);
