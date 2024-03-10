93addf3a1e2114edfe9248ea0328d606214fc8b2
SemihGuner <semihgs_890@hotmail.com>
2024-03-10 14:42:31 +0300

    asd

---
+++ Assets/Scripts/ToggleController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class ToggleController : MonoBehaviour
+{      
+    public void Togglelaninca()
+    {
+        
+    }
+}
