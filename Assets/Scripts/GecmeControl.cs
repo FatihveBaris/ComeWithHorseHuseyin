4a4785b7590c9e0af70954bc26c91e079ea96579
FatihveBaris <117710225+FatihveBaris@users.noreply.github.com>
2024-03-10 17:59:06 +0300

    gecme cont

---
+++ Assets/Scripts/GecmeControl.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GecmeControl : MonoBehaviour
+{
+    public List<GameObject> gameObjects = new List<GameObject>();
+    private float counter = 0f;
+
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        if (counter >= 12f)
+        {
+            counter = 0f;
+            SortGameObjects();
+        }
+    }
+
+    void SortGameObjects()
+  {
+        // Sort the gameObjects list based on x positions
+        gameObjects.Sort((a, b) => b.transform.position.x.CompareTo(a.transform.position.x));
+
+        // Display the order in the debug log
+        for (int i = 0; i < gameObjects.Count; i++)
+        {
+            Debug.Log($"{i + 1}. at: x = {gameObjects[i].name}");
+        }
+    }
+
+}
